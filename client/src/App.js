5936cc9cd3418b833b428ef1d051de5cfabda076
jasmineakbari <jasmineakbari@yahoo.com>
2022-02-13 17:14:08 -0500

    needs auth/login/signup page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import { useState } from "react";
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="playerInfoDisplay"></div>
     </div>
   );
 }
